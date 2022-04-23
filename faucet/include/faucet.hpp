3549cdb8459edff6867967e4ba2f1676d07bc589
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2022-04-23 10:54:02 -0600

    feat(faucet): change from clsdk to cdt

--- faucet/include/faucet.hpp
+++ faucet/include/faucet.hpp
@@ -1,51 +1,32 @@
-#pragma once
-
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
-
-#include "../ricardian/faucet_ricardian.hpp"
 
 using namespace std;
 using namespace eosio;
 
-namespace faucets {
-  const int64_t TOKENS_PER_REQUEST = 500;
-  const int64_t TOKEN_PRECISION = 8;
-  const std::string TOKEN_SYMBOL = "UOS";
+CONTRACT faucet_contract : public contract {
+  public:
+    using contract::contract;
 
-  struct faucet {
-    name account;
-    eosio::time_point_sec interval;
-    uint32_t max_tokens_per_interval;
-    eosio::time_point until;
-    uint32_t transferred_tokens;
+    const int64_t TOKENS_PER_REQUEST = 500;
+    const int64_t TOKEN_PRECISION = 8;
+    const std::string TOKEN_SYMBOL = "UOS";
 
-    uint64_t primary_key() const { return account.value; }
-  };
-  EOSIO_REFLECT(
-    faucet,
-    account,
-    interval,
-    max_tokens_per_interval,
-    until,
-    transferred_tokens
-  )
-  typedef eosio::multi_index<"faucets"_n, faucet> faucet_table;
+    // Admin only
+    ACTION addfaucet(name account, eosio::time_point_sec interval, uint32_t max_tokens_per_interval);
+    ACTION rmfaucet(name account);
 
-  class contract : public eosio::contract {
-    public:
-      using eosio::contract::contract;
+    // Faucet only
+    ACTION givetokens(name faucet, name to);
+  private: 
+    TABLE faucet {
+      name                      account;
+      eosio::time_point_sec     interval;
+      uint32_t                  max_tokens_per_interval;
+      eosio::time_point         until;
+      uint32_t                  transferred_tokens;
 
-      // Admin only
-      void addfaucet(name account, eosio::time_point_sec interval, uint32_t max_tokens_per_interval);
-      void rmfaucet(name account);
-
-      // Faucet only
-      void givetokens(name faucet, name to);
-  };
-
-  EOSIO_ACTIONS(contract, "faucet"_n,
-                action(addfaucet, account, interval, max_tokens_per_interval),
-                action(rmfaucet, account),
-                action(givetokens, faucet, to))
-} // namespace faucets
+      uint64_t primary_key() const { return account.value; }
+    };
+    typedef eosio::multi_index<"faucets"_n, faucet> faucet_table;
+};
--- faucet/src/faucet.cpp
+++ faucet/src/faucet.cpp
@@ -1,84 +1,70 @@
+#include <faucet.hpp>
 #include <eosio/system.hpp>
 #include <cmath>
 
-#include "../include/faucet.hpp"
+ACTION faucet_contract::addfaucet(name account, eosio::time_point_sec interval, uint32_t max_tokens_per_interval) {
+  require_auth(get_self());
 
-namespace faucets {
-  void contract::addfaucet(name account, eosio::time_point_sec interval, uint32_t max_tokens_per_interval) {
-    require_auth(get_self());
+  faucet_table _faucet(get_self(), get_self().value);
+  auto faucet_itr = _faucet.find(account.value);
 
-    faucet_table _faucet(get_self(), get_self().value);
-    auto faucet_itr = _faucet.find(account.value);
+  check(faucet_itr == _faucet.end(), "Faucet account is already registered");
 
-    check(faucet_itr == _faucet.end(), "Faucet account is already registered");
+  _faucet.emplace(get_self(), [&](auto& row) {
+    row.account = account;
+    row.interval = interval;
+    row.max_tokens_per_interval = max_tokens_per_interval;
+    row.until = current_time_point() + seconds(interval.sec_since_epoch());
+    row.transferred_tokens = 0;
+  });
+}
 
-    _faucet.emplace(get_self(), [&](auto& row) {
-      row.account = account;
-      row.interval = interval;
-      row.max_tokens_per_interval = max_tokens_per_interval;
-      row.until = current_time_point() + seconds(interval.sec_since_epoch());
+ACTION faucet_contract::rmfaucet(name account) {
+  require_auth(get_self());
+
+  faucet_table _faucet(get_self(), get_self().value);
+  auto faucet_itr = _faucet.find(account.value);
+
+  check(faucet_itr != _faucet.end(), "Faucet does not exist");
+
+  _faucet.erase(faucet_itr);
+}
+
+ACTION faucet_contract::givetokens(name faucet, name to) {
+  require_auth(faucet);
+
+  faucet_table _faucet(get_self(), get_self().value);
+  auto faucet_itr = _faucet.find(faucet.value);
+
+  check(faucet_itr != _faucet.end(), "Faucet does not exist");
+
+  time_point now = current_time_point();
+  if(now > faucet_itr->until) {
+    uint32_t intervalSecs = faucet_itr->interval.sec_since_epoch();
+    float windowGap = (now.sec_since_epoch() - faucet_itr->until.sec_since_epoch()) / intervalSecs;
+    
+    time_point newUntil = faucet_itr->until + seconds(intervalSecs) + time_point(seconds(floor(windowGap) * intervalSecs));
+
+    _faucet.modify(faucet_itr, get_self(), [&]( auto& row ) {
+      row.until = newUntil;
       row.transferred_tokens = 0;
     });
   }
 
-  void contract::rmfaucet(name account) {
-    require_auth(get_self());
+  check( now <= faucet_itr->until &&
+    faucet_itr->transferred_tokens + TOKENS_PER_REQUEST <= faucet_itr->max_tokens_per_interval, "Not so fast...");
 
-    faucet_table _faucet(get_self(), get_self().value);
-    auto faucet_itr = _faucet.find(account.value);
+  _faucet.modify(faucet_itr, get_self(), [&]( auto& row ) {
+    row.transferred_tokens = faucet_itr->transferred_tokens + TOKENS_PER_REQUEST;
+  });
 
-    check(faucet_itr != _faucet.end(), "Faucet does not exist");
+  asset amount = asset(TOKENS_PER_REQUEST * pow(10, TOKEN_PRECISION), symbol(TOKEN_SYMBOL, TOKEN_PRECISION));
+  string memo = "Faucet transfer";
 
-    _faucet.erase(faucet_itr);
-  }
-
-  void contract::givetokens(name faucet, name to) {
-    require_auth(faucet);
-
-    faucet_table _faucet(get_self(), get_self().value);
-    auto faucet_itr = _faucet.find(faucet.value);
-
-    check(faucet_itr != _faucet.end(), "Faucet does not exist");
-
-    time_point now = current_time_point();
-    if(now > faucet_itr->until) {
-      uint32_t intervalSecs = faucet_itr->interval.sec_since_epoch();
-      float windowGap = (now.sec_since_epoch() - faucet_itr->until.sec_since_epoch()) / intervalSecs;
-      
-      time_point newUntil = faucet_itr->until + seconds(intervalSecs) + time_point(seconds(floor(windowGap) * intervalSecs));
-
-      _faucet.modify(faucet_itr, get_self(), [&]( auto& row ) {
-        row.until = newUntil;
-        row.transferred_tokens = 0;
-      });
-    }
-
-    check( now <= faucet_itr->until &&
-      faucet_itr->transferred_tokens + TOKENS_PER_REQUEST <= faucet_itr->max_tokens_per_interval, "Not so fast...");
-
-    _faucet.modify(faucet_itr, get_self(), [&]( auto& row ) {
-      row.transferred_tokens = faucet_itr->transferred_tokens + TOKENS_PER_REQUEST;
-    });
-
-    asset amount = asset(TOKENS_PER_REQUEST * pow(10, TOKEN_PRECISION), symbol(TOKEN_SYMBOL, TOKEN_PRECISION));
-    string memo = "Faucet transfer";
-
-    eosio::action(
-      permission_level {get_self(), "transferer"_n},
-      "eosio.token"_n,
-      "transfer"_n,
-      std::make_tuple(get_self(), to, amount, memo)
-    ).send();
-  }
-} // namespace faucets
-
-EOSIO_ACTION_DISPATCHER(faucets::actions)
-EOSIO_ABIGEN(actions(faucets::actions),
-             table("faucets"_n, faucets::faucet),
-             ricardian_clause("datastorage", faucets::datastorage_clause),
-             ricardian_clause("datausage", faucets::datausage_clause),
-             ricardian_clause("dataownership",
-                              faucets::dataownership_clause),
-             ricardian_clause("datadistribution",
-                              faucets::datadistribution_clause),
-             ricardian_clause("datafuture", faucets::datafuture_clause))
+  eosio::action(
+    permission_level {get_self(), "transferer"_n},
+    "eosio.token"_n,
+    "transfer"_n,
+    std::make_tuple(get_self(), to, amount, memo)
+  ).send();
+}
